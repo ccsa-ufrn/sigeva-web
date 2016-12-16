6045c867a2c53ae9fcf0df451438b1a238d1d356
Victor Almeida Schinaider <victorschinaider@outlook.com>
2016-12-16 17:17:21 -0300

    first commit

---
+++ source/ts/dashboard/dashboard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app',
+    styleUrls: [],
+    template: `
+        <h1>Dashboard</h1>
+    `
+})
+export class DashboardComponent { }
---
+++ source/ts/external/authorization/login.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'login',
+    styleUrls: ['public/css/login.css'],
+    templateUrl: 'public/templates/login.html'
+})
+export class LoginComponent { }
