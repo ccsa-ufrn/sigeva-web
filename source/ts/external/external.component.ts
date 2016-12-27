0b2baa0f6f777780158ed1b3f234d9d010b320eb
Victor Almeida Schinaider <victorschinaider@outlook.com>
2016-12-27 14:38:20 -0300

    updates

--- source/ts/external/external.component.ts
+++ source/ts/external/external.component.ts
@@ -3,38 +3,6 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app',
     styleUrls: [],
-    template: `
-
-        <!-- Ajeitar -->
-        <nav class="navbar navbar-fixed-top navbar-light" style="background-color: white; border-bottom: 1px solid #c6c6c6;">
-            <a class="navbar-brand" href="#">SIGEVA</a>
-            <div class="float-xs-right">
-                <ul class="nav navbar-nav">
-                    <li class="nav-item">
-                        <a
-                            class="nav-link"
-                            routerLink="/"
-                        >Início</a>
-                    </li>
-                    <li class="nav-item">
-                        <a
-                            class="nav-link text-danger"
-                            routerLink="/register"
-                        >Inscrever-se</a>
-                    </li>
-                    <li class="nav-item ">
-                        <a
-                            class="nav-link text-success"
-                            routerLink="/login"
-                        >Entrar</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-
-        <div class="container">
-            <router-outlet></router-outlet>
-        </div>
-    `
+    templateUrl: 'public/templates/external.html'
 })
 export class ExternalComponent { }
--- source/ts/external/home.component.ts
+++ source/ts/external/home.component.ts
@@ -1,41 +1,21 @@
-import { Component } from '@angular/core';
+import { 
+    Component, 
+    ViewChild,
+    ElementRef,
+    OnInit
+} from '@angular/core';
 
 @Component({
     selector: 'app',
     styleUrls: ['public/css/home.css'],
-    template: `
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="bg-image">
-                    <h1>SIGEVA</h1>
-                    <h2>Participe da comunidade acadêmica através dos eventos
-                    que nós oferecemos à você.</h2>
-                </div>
-
-                <div class="main-box">
-                    <input class="form-control" placeholder="Procurar evento">
-                    <button>Cadastrar-se</button>
-                </div>
-            </div>
-        </div>
-
-        <div class="row box-events">
-            <div class="col-lg-12">
-                <h1><span>#</span> Acompanhe nossos eventos</h1>
-
-                <table>
-                    <tr>
-                        <td class="event-name">XXI Seminário de Pesquisa do CCSA</td>
-                        <td class="event-status">
-                            <span>Inscrições Abertas</span>
-                        </td>
-                        <td class="event-register">
-                            <a>Inscrever-se</a>
-                        </td>
-                    </tr>
-                </table>
-            </div>
-        </div>
-    `
+    templateUrl: 'public/templates/home.html'
 })
-export class HomeComponent { }
+export class HomeComponent implements OnInit { 
+
+    @ViewChild('searchEventInput') searchEventInput: ElementRef;
+
+    ngOnInit() {
+        this.searchEventInput.nativeElement.focus();
+    }    
+
+}
